a28d29289e0cf418c294a8d792399251ff3c887e
Goluchy6249 <sampurnaseo@gmail.com>
2025-06-13 20:26:58 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import SalesReport from './SalesReport';
+
+function App() {
+  return (
+    <div className="App">
+      <SalesReport />
+    </div>
+  );
+}
+
+export default App;
